d54a52933046ab3738844c55c8cae1a78c404d06
Scott Branch <scott.branch@credera.com>
2022-11-08 21:43:53 -0800

    fonts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "97490145cd33cb9f"; }
+/******/ 		__webpack_require__.h = function() { return "7392ef7f44bf3869"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
