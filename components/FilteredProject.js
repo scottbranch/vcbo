224d9a87f09c9fa5410efd0d762e7e6af7860dad
Scott Branch <scott.branch@credera.com>
2022-12-17 13:52:29 -0800

    fixing color thing

--- components/FilteredProject.js
+++ components/FilteredProject.js
@@ -1,5 +1,4 @@
 import { Button } from "./Button"
-import Link from "next/link"
 
 export const FilteredProject = (props) => {
   const { title, specialty, image, url, className } = props
@@ -7,7 +6,7 @@ export const FilteredProject = (props) => {
     <div
       className={`filtered-project project-col col-span-2 md:col-span-1 ${className}`}
     >
-      <Link href={url} className="project-wrapper">
+      <a href={url} className="project-wrapper">
         <img className="w-full" mb-4 src={image} />
         <div className="flex justify-between my-5">
           <div className="w-80">
@@ -15,7 +14,7 @@ export const FilteredProject = (props) => {
             <p className="project-specialty">{specialty}</p>
           </div>
         </div>
-      </Link>
+      </a>
     </div>
   )
 }
