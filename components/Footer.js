ef14dde8e825bd1b901573a46f880f78a9bd0292
Scott Branch <scott.branch@credera.com>
2022-12-18 20:39:20 -0800

    footer responsive fix

--- components/Footer.js
+++ components/Footer.js
@@ -6,26 +6,26 @@ export const Footer = (props) => {
     <div className="container mx-auto grid mt-40">
       <BackToTop />
       <footer className="relative mt-28 md:mt-40 pb-10 px-4 md:px-0">
-        <div className="flex flex-col md:flex-row grid-cols-4 md:justify-between">
-          <div className="grid grid-cols-4 mb-20 md:grid-cols-1 md:mr-20">
+        <div className="flex flex-col md:grid grid-cols-4">
+          <div className="grid grid-cols-4 mb-20 md:grid-cols-1 md:col-span-2 md:mr-20">
             <h3 className="col-start-1 col-span-2 md:col-span-1 md:grid-cols-1">
               Spaces for people to thrive.
             </h3>
           </div>
-          <div className="grid grid-cols-4 items-center">
-            <div className="col-span-4 grid">
+          <div className="grid grid-cols-4 items-center md:col-span-2">
+            <div className="grid-cols-4 col-span-4 grid">
               <div>
                 <p className="small-subhead mb-4">Inquiries</p>
                 <p>info@vcbo.com</p>
                 <p>+1 801 575 8800</p>
               </div>
-              <div className="col-start-3 col-span-2 md:ml-5">
+              <div className="col-start-3 col-span-2">
                 <p className="small-subhead mb-4">SLC (HQ)</p>
                 <p>524 South 600 East</p>
                 <p>Salt Lake City, UT 84102</p>
               </div>
             </div>
-            <div className="col-span-4 grid mt-10">
+            <div className="grid-cols-4 col-span-4 grid mt-10">
               <div>
                 <p className="small-subhead mb-4">Careers</p>
                 <p>Create something great.</p>
@@ -35,7 +35,7 @@ export const Footer = (props) => {
                   </a>
                 </p>
               </div>
-              <div className="col-start-3 col-span-2 md:ml-5">
+              <div className="col-start-3 col-span-2">
                 <p className="small-subhead mb-4">STG</p>
                 <p>20 North Main Street, Suite 103</p>
                 <p>St. George, UT 84770</p>
