836a9bd68fde0fa4056a9e44957ff19ed7f8ac82
Scott Branch <scott.branch@credera.com>
2023-03-23 11:18:12 -0700

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -7,26 +7,48 @@ import { PrismicProvider } from "@prismicio/react"
 import { PrismicPreview } from "@prismicio/next"
 import { repositoryName } from "../prismicio"
 import Head from "next/head"
+import Script from "next/script"
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
-      <PrismicPreview repositoryName={repositoryName}>
-        <Head>
-          <title>
-            VCBO | Architecture, Interiors, Planning, and Sustainability
-          </title>
-          <meta
-            name="description"
-            content="VCBO | Architecture, Interiors, Planning, and Sustainability"
-          />
-        </Head>
-        <div className="page-wrapper relative">
-          <Header />
-          <Component {...pageProps} />
-          <Footer data={pageProps?.footer?.data} />
-        </div>
-      </PrismicPreview>
-    </PrismicProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-F963Y1THH7"
+      />
+          
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-F963Y1THH7', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+        <PrismicPreview repositoryName={repositoryName}>
+          <Head>
+            <title>
+              VCBO | Architecture, Interiors, Planning, and Sustainability
+            </title>
+            <meta
+              name="description"
+              content="VCBO | Architecture, Interiors, Planning, and Sustainability"
+            />
+          </Head>
+          <div className="page-wrapper relative">
+            <Header />
+            <Component {...pageProps} />
+            <Footer data={pageProps?.footer?.data} />
+          </div>
+        </PrismicPreview>
+      </PrismicProvider>
+    </>
   )
 }
