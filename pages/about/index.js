ad63e4817add0e93edee8c1afee7ba0136bbfbe1
Scott Branch <scott.branch@credera.com>
2023-03-23 11:56:16 -0700

    list updates

--- pages/about/index.js
+++ pages/about/index.js
@@ -55,7 +55,7 @@ export default function About(props) {
         />
       </Head>
       <Lines loaded={loaded} />
-      <div className="container mx-auto px-4 md:px-0 mb-60">
+      <div className="about-page container mx-auto px-4 md:px-0 mb-60">
         <div className="grid grid-cols-8">
           <div className="col-span-12 md:col-span-6">
             <ScrollAnimate>
